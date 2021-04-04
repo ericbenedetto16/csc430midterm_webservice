7b3ef1fa56d28bc12e36b25fa97cddb5bddcdcbc
Eric Benedetto <ericsplace3@gmail.com>
2021-04-04 17:37:43 -0400

    add parent relationship

--- src/main/java/edu/cuny/csi/csc430/professionalGooglersAPI/db/models/Student.java
+++ src/main/java/edu/cuny/csi/csc430/professionalGooglersAPI/db/models/Student.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -17,6 +18,10 @@ public class Student {
 	@JoinColumn(nullable = false)
 	@OneToOne
 	private User user;
+	
+	@JoinColumn(nullable = false)
+	@ManyToOne
+	private User parent;
 	
 	@Column(nullable = false)
 	private Integer grade;
@@ -35,6 +40,14 @@ public class Student {
 	
 	public void setUser(User user) {
 		this.user = user;
+	}
+	
+	public User getParent() {
+		return parent;
+	}
+	
+	public void setParent(User parent) {
+		this.parent = parent;
 	}
 	
 	public Integer getGrade() {
