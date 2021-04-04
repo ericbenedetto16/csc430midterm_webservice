6d65ba6b16c4b3ca17da3b6cda6ac99e3f67ee60
Eric Benedetto <ericsplace3@gmail.com>
2021-04-03 22:01:40 -0400

    add utils and seeders

--- src/main/java/edu/cuny/csi/csc430/professionalGooglersAPI/ProfessionalGooglersApiApplication.java
+++ src/main/java/edu/cuny/csi/csc430/professionalGooglersAPI/ProfessionalGooglersApiApplication.java
@@ -1,13 +1,110 @@
 package edu.cuny.csi.csc430.professionalGooglersAPI;
 
+import java.security.SecureRandom;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import edu.cuny.csi.csc430.professionalGooglersAPI.api.repositories.FacultyRepository;
+import edu.cuny.csi.csc430.professionalGooglersAPI.api.repositories.RoleRepository;
+import edu.cuny.csi.csc430.professionalGooglersAPI.api.repositories.UserRepository;
+import edu.cuny.csi.csc430.professionalGooglersAPI.api.utils.APIUtils;
+import edu.cuny.csi.csc430.professionalGooglersAPI.api.utils.HasherObject;
+import edu.cuny.csi.csc430.professionalGooglersAPI.api.utils.Roles;
+import edu.cuny.csi.csc430.professionalGooglersAPI.db.models.Faculty;
+import edu.cuny.csi.csc430.professionalGooglersAPI.db.models.Role;
+import edu.cuny.csi.csc430.professionalGooglersAPI.db.models.User;
 
 @SpringBootApplication
 public class ProfessionalGooglersApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessionalGooglersApiApplication.class, args);
 	}
+	
+	@Bean
+	CommandLineRunner seeders(RoleRepository roleRepo, UserRepository userRepo, FacultyRepository facultyRepo) {
+		return (args) -> {
+			// Run Seeders
+			Role adminRole = roleRepo.findByRoleName(Roles.ADMIN.getRole());
+			if(adminRole == null) {
+				adminRole = new Role();
+				adminRole.setName(Roles.ADMIN.getRole());
+				adminRole = roleRepo.save(adminRole);
+			}
+			
+			Role parentRole = roleRepo.findByRoleName(Roles.PARENT.getRole());
+			if(parentRole == null) {
+				parentRole = new Role();
+				parentRole.setName(Roles.PARENT.getRole());
+				parentRole = roleRepo.save(parentRole);
+			}
+			
+			Role facultyRole = roleRepo.findByRoleName(Roles.FACULTY.getRole());
+			if(facultyRole == null) {
+				facultyRole = new Role();
+				facultyRole.setName(Roles.FACULTY.getRole());
+				facultyRole = roleRepo.save(facultyRole);
+			}
+			
+			Role studentRole = roleRepo.findByRoleName(Roles.STUDENT.getRole());
+			if(studentRole == null) {
+				studentRole = new Role();
+				studentRole.setName(Roles.STUDENT.getRole());
+				studentRole = roleRepo.save(studentRole);
+			}
+			
+			User adminUser = userRepo.findByFirstNameAndLastName("Admin", "Istrator");
+			if(adminUser == null) {
+				adminUser = new User();
+				adminUser.setEmail("admin@schoolsystem.com");
+				adminUser.setFirstName("Admin");
+				adminUser.setLastName("Istrator");
+				
+				SecureRandom random = new SecureRandom();
+				byte[] salt = new byte[16];
+				random.nextBytes(salt);
+				
+				String password = "supersecurepassword";
+				HasherObject encoded = APIUtils.hasher(salt, password);
 
+				adminUser.setSalt(encoded.getEncodedSalt());
+				adminUser.setPassword(encoded.getEncodedHash());
+				
+				adminUser.setRole(adminRole);
+				adminUser = userRepo.save(adminUser);
+			}
+			
+			User teacherUser = userRepo.findByFirstNameAndLastName("Teacher", "Dude");
+			if(teacherUser == null) {
+				teacherUser = new User();
+				teacherUser.setEmail("teacherdude@schoolsystem.com");
+				teacherUser.setFirstName("Teacher");
+				teacherUser.setLastName("Dude");
+				
+				SecureRandom random = new SecureRandom();
+				byte[] salt = new byte[16];
+				random.nextBytes(salt);
+				
+				String password = "teacherpassword";
+				HasherObject encoded = APIUtils.hasher(salt, password);
+				
+				teacherUser.setSalt(encoded.getEncodedSalt());
+				teacherUser.setPassword(encoded.getEncodedHash());
+				
+				teacherUser.setRole(facultyRole);
+				teacherUser = userRepo.save(teacherUser);
+			}
+			
+			Faculty teacherFaculty = facultyRepo.findByUserId(teacherUser.getId());
+			if(teacherFaculty == null) {
+				teacherFaculty = new Faculty();
+				teacherFaculty.setUser(teacherUser);
+				teacherFaculty.setSalary((double) 65000);
+				
+				teacherFaculty = facultyRepo.save(teacherFaculty);
+			}
+		};
+	}
 }
---
+++ src/main/java/edu/cuny/csi/csc430/professionalGooglersAPI/api/utils/APIUtils.java
@@ -0,0 +1,53 @@
+package edu.cuny.csi.csc430.professionalGooglersAPI.api.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class APIUtils {
+	public static final String LOGGED_OUT_JSON = "{ \"success\": false, \"msg\": \"You Must Be Logged In to Access This Resource\"";
+	public static final String UNAUTHORIZED_JSON = "{ \"success\": false, \"msg\": \"You Are Not Authorized to Access This Resource\"";
+	
+	public static HasherObject hasher(byte[] salt, String password) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			md.update(salt);
+			
+			byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+			
+			String encodedSalt = Base64.getEncoder().encodeToString(salt);
+			String encodedHash = Base64.getEncoder().encodeToString(hash);
+			
+			return new HasherObject(encodedSalt, encodedHash);
+		}catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return new HasherObject("", "");
+		}
+	}
+	
+	public static String createToken() {
+		SecureRandom secureRandom = new SecureRandom();
+		byte[] randomBytes = new byte[24];
+	    secureRandom.nextBytes(randomBytes);
+	    
+	    return Base64.getEncoder().encodeToString(randomBytes);
+	}
+	
+	public static String baseJSON(Boolean success, String secondAttribute) {
+		return "{ \"success\": " + success + ", \"" + secondAttribute + "\":";
+	}
+	
+	public static String baseJSON(Boolean success, String secondAttribute, Object o) {
+		try {
+			return "{ \"success\": " + success + ", \"" + secondAttribute + "\":" + new ObjectMapper().writeValueAsString(o);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			return baseJSON(false, "msg") + "Error While Parsing JSON";
+		}
+	}
+}
